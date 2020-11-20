f44a61edcd41522a4739415e6719fe786ac7b1fd
shanelbert <shanelbert@yahoo.com>
2020-11-20 18:49:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-// const axios = require('axios');
 
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL || "postgres://hlufkqnkonmtjn:0c22bc61c87916686b52a97d8549ca243258234bd4b0d8922ddffbd1beb79152@ec2-54-75-150-32.eu-west-1.compute.amazonaws.com:5432/d61cup49q5ghaj",
 	ssl: { rejectUnauthorized: false }
 });
 
-// function handleSubmit() {
-// 	let judul = document.getElementById("inputJudul");
-// 	let tipe = document.getElementById("inputTipe");
-// 	let lorong = document.getElementById("inputLorong");
-// 	let rak = document.getElementById("inputRak");
-
-
-// 	axios.get('/collection/location').then((res) => {
-// 		alert(res);
-// 	});
-// }
-
-
 express()
 	.use(bodyParser.json())
-	// .get('/', async (req, res) => {
-		// let htmlForm = `
-		// 	<!DOCTYPE html>
-		// 	<html>
-		// 		<head>
-		// 			<title>Input Form</title>
-		// 		</head>
-		// 		<body>
-		// 			<h2>Input Data Koleksi</h2>
-
-					
-		// 				<label for="inputJudul">Judul:</label><br>
-		// 				<input type="text" id="inputJudul" name="koleksi"><br>
-
-		// 				<label for="inputTipe">Tipe:</label><br>
-		// 				<input type="text" id="inputTipe" name="koleksi"><br><br>
-
-		// 				<label for="inputLorong">Nomor Lorong:</label><br>
-		// 				<input type="text" id="inputLorong" name="koleksi"><br>
-
-		// 				<label for="inputRak">Nomor Rak:</label><br>
-		// 				<input type="text" id="inputRak" name="koleksi"><br>
-
-		// 				<button onclick="handleSubmit()">
-					
-		// 		</body>
-		// 	</html>`
-	// })
-	.get('/collection/location', async (req, res) => {
+	.get('/collection/loc', async (req, res) => {
 		try {
 			const client = await pool.connect();
-			let result;
-			client.query(`SELECT no_lorong, no_rak FROM collection WHERE id=${req.query.id};`, (err, res) => {
+			client.query(`SELECT no_lorong, no_rak FROM collection WHERE id=${req.query.id};`, (err, queryresult) => {
 				if (err) {
 					throw err;
 				} else {
-					result = res.rows;
+					client.release();
+					res.json(queryresult.rows[0]);
 				}
 			});
-			res.json(result);
-			client.release();
-			// res.json({lorong: '1', rak: '2'});
 		} catch (err) {
-			// console.error(err);
 			res.status(500).send('Error ' + err);
 		}
 	})
-	.post('/collection/new', async (req, res) => {
+	.post('/collection', async (req, res) => {
 		try {
 			const client = await pool.connect();
 			const { judul, tipe, no_lorong, no_rak } = req.body; 
-			client.query(`INSERT INTO collection(judul, tipe, no_lorong, no_rak) VALUES (${judul}, ${tipe}, ${no_lorong}, ${no_rak});`, (err) => {
+			client.query(`INSERT INTO collection(judul, tipe, no_lorong, no_rak) VALUES ('${judul}', '${tipe}', '${no_lorong}', '${no_rak}');`, (err) => {
 				if (err) {
 					throw err;
 				}
 			});
-
-			res.json(body.res);
 			client.release();
+			res.json(req.body);
 		} catch (err) {
-			// console.error(err);
 			res.status(500).send('Error ' + err);
 		}
 	})
